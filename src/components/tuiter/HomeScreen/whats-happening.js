ce3572e0fc0e90833563677ef3b5490cc55b9ee7
Jiayi <mmajiayi@163.com>
2022-04-04 17:29:21 -0400

    6.4 with style issue

--- src/components/tuiter/HomeScreen/whats-happening.js
+++ src/components/tuiter/HomeScreen/whats-happening.js
@@ -1,19 +1,21 @@
 import React, {useState} from "react";
 import {useDispatch}from "react-redux";
+
+import{createTuit} from "../../actions/tuits-actions";
+
 const WhatsHappening = () => {
-    let [whatsHappening, setWhatsHappening]
-        = useState('');
+
+    const [newTuit, setNewTuit] =
+        useState({tuit: 'New tuit'});
+
     const dispatch = useDispatch();
-    const tuitClickHandler = () => {
-        dispatch({type: 'create-tuit',
-            tuit: whatsHappening
-        });
-    }
+
     return ( <>
         <div className="d-flex">
             <img src="/tuiter/images/elonmusk.jpeg" alt="" className="wd-profile"/>
-            <textarea className="wd-textarea ms-2" placeholder="What's happening?" value={whatsHappening} onChange={(event) =>
-                setWhatsHappening(event.target.value)}>
+            <textarea className="wd-textarea ms-2 form-control" placeholder="What's happening?" onChange={(e) =>
+                setNewTuit({...newTuit,
+                    tuit: e.target.value})}>
             </textarea>
         </div>
         <div className="d-flex">
@@ -25,7 +27,8 @@ const WhatsHappening = () => {
                 </div>
 
                 <div className="wd-tweet mt-2 mb-3">
-                    <button className="btn btn-primary btn-block rounded-pill " onClick={tuitClickHandler}> Tweet </button>
+                    <button className="btn btn-primary btn-block rounded-pill " onClick={() =>
+                        createTuit(dispatch, newTuit)}> Tuit </button>
                 </div>
             </div>
 
--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,7 +1,12 @@
-import {FIND_ALL_TUITS, DELETE_TUIT} from "../../actions/tuits-actions";
+import {FIND_ALL_TUITS, DELETE_TUIT,CREATE_TUIT} from "../../actions/tuits-actions";
 
 const tuitsReducer = (state = [], action) => {
     switch (action.type) {
+        case CREATE_TUIT:
+            return [
+                ...state,
+                action.newTuit
+            ];
         case FIND_ALL_TUITS:
             return action.tuits;
         case DELETE_TUIT:
